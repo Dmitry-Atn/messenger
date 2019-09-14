9bb189e7cafefb3b819ea39e1f2c1bcd3c961807
Dmitry Atanov <atanov.dmitry@gmail.com>
2019-09-14 18:09:31 +0300

    settings.py fix

--- api_messenger/api_messenger/settings.py
+++ api_messenger/api_messenger/settings.py
@@ -14,7 +14,6 @@ import os
 import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-django_heroku.settings(locals())
 
 
 # Quick-start development settings - unsuitable for production
@@ -135,3 +134,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+django_heroku.settings(locals())
