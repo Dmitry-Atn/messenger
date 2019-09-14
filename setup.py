63407bf25031724daffe795b30991083af7bdda4
Dmitry Atanov <atanov.dmitry@gmail.com>
2019-09-14 11:26:50 +0300

    requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='messenger2020',
+    version='',
+    packages=['api_messenger.messenger', 'api_messenger.messenger.migrations', 'api_messenger.api_messenger'],
+    url='',
+    license='',
+    author='atano',
+    author_email='',
+    description=''
+)
